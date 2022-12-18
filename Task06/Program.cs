33d4ec07a00d75b47297951c784fed817266e16a
Станислав <sts55@mail.ru>
2022-12-18 18:29:30 +0300

    Исправил Task6

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -9,5 +9,5 @@ int number = Convert.ToInt32(Console.ReadLine());
 if(number % 2 == 0) Console.WriteLine($"Число {number} - четное");
 else
 {
-    Console.WriteLine($"Число {number} - не четное");
+    Console.WriteLine("Нет");
 }
