f90d9e0b48661e47a8d3b403b0f464c81ffab434
Станислав <sts55@mail.ru>
2022-12-18 16:57:06 +0300

    add Task07

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -4,7 +4,7 @@
 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 2 -> " -2, -1, 0, 1, 2" */
 
-Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = -number;
 while (count <= number)
