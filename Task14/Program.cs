24ab3931fb8b4e96355544ec57acc39ff1945003
Станислав <sts55@mail.ru>
2022-12-24 15:18:57 +0300

    add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 14. Напишите программу, которая принимает на
+вход число и проверяет, кратно ли оно
+одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да  
+? - тогда
+: - иначе                  */
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 7;
+int numberC = 23;
+
+Console.WriteLine(Multiplicity(numberA, numberB, numberC) ? $"{numberA} -> да" : $"{numberA} -> нет");
+
+bool Multiplicity(int numA, int numB, int numC)
+{
+     return numA % numB == 0 && numA % numC == 0;
+} 
+
+/*
+if (numberA % numberB == 0 && numberA % numberC == 0) 
+{
+    Console.WriteLine($"{numberA} -> да");
+}
+
+else Console.WriteLine($"{numberA} -> нет");*/
+
---
+++ Task16/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+16. Напишите программу, которая принимает на
+вход два числа и проверяет, является ли одно
+число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет */
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+bool(numberA % numberB || numberB % numberA == 0);
+if (bool)
+
+{
+    Console.WriteLine($"{numberA},{NumberB} -> yes");
+}
+else Console.WriteLine($"{numberA},{NumberB} -> no");
