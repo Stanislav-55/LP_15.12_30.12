6537e088c17a0d29e513a1eadb14c25c5703ae82
Станислав <sts55@mail.ru>
2023-01-07 14:55:24 +0300

    add Task19

---
+++ Task22/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 22: Напишите программу, которая
+принимает на вход число (N) и выдаёт таблицу
+квадратов чисел от 1 до N.*/
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number>0)
+{
+    TableSquare(number);
+}
+else  Console.WriteLine("данное число не натуральное");
+
+void TableSquare (int num)
+{int count = 1;
+while (count <= num)
+{
+   double square =Math.Pow(count, 2);
+    Console.WriteLine($"{count,4} {square,4} ");    //    \t - длинный отступ      ,4- отступ от края 
+
+ count++;
+}
+}
+
