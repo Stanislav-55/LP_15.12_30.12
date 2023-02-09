00d8cb9e3f760902264ad5f2085057ee7a286742
Станислав <sts55@mail.ru>
2023-02-09 20:34:31 +0300

    add Task53

---
+++ Task53/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Задача 53: Задайте двумерный массив. Напишите программу,
+которая поменяет местами первую и последнюю строку
+массива.*/
+
+
+int[,] matrix = CreateMatrix(4, 4, -99, 99);
+PrintMatrix(matrix);
+
+ChangeFirstAndLastRows(matrix);
+Console.WriteLine(string.Empty);
+PrintMatrix(matrix);
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void ChangeFirstAndLastRows(int[,] matrixForChange)
+{
+    int firstRow = 0;
+    int lastRow = matrixForChange.GetLength(0) - 1;
+    for (int i = 0; i < matrixForChange.GetLength(1); i++)
+    {
+        int temp = matrixForChange[firstRow, i];
+        matrixForChange[firstRow, i] = matrixForChange[lastRow, i];
+        matrixForChange[lastRow,i]=temp;
+    }
+}
